d563668488a43ad4d97666f879f7e39c5621fac5
Justin Navarro <info@justinnavarro.net>
2019-06-30 16:32:45 -0400

    created store provider in the MyApp build

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,20 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
-		return MaterialApp(
-			title: 'Flutter Demo',
-			theme: ThemeData(
-				primarySwatch: Colors.blue,
+		final Store<AppState> store = Store<AppState>(
+			appStateReducer,
+			initialState: AppState.initialState(),
+		);
+
+		return StoreProvider<AppState>(
+			store: store,
+			child: MaterialApp(
+				title: 'Flutter Demo',
+				theme: ThemeData(
+					primarySwatch: Colors.blue,
+				),
+				home: Home(),
 			),
-			home: Home(),
 		);
 	}
 }
